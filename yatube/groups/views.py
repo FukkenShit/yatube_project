e5e9e80b0a0366fa400daa9d37eb84648351b977
Danila Kamaev <danila@kamaeff.ru>
2021-12-22 04:06:44 +0300

    added views for posts and groups

--- yatube/groups/views.py
+++ yatube/groups/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def group_posts(request, slug):
-    return 'Group view'
+    return HttpResponse(f'Группа {slug}')
