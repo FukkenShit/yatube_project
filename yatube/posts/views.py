a087f20aadf127b90d3b5d8b0768bd7a14efc24c
Danila Kamaev <danila@kamaeff.ru>
2021-12-31 22:40:34 +0300

    Разбил html на шаблоны

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'posts/index.html')
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')
+    return render(request, 'posts/group_list.html')
