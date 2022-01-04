a4a7b1e8559c0e76a0cdb58f7248db52b9986803
Danila Kamaev <danila@kamaeff.ru>
2022-01-04 17:50:45 +0300

    добавил всякое

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
     template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': 'Это главная страница проекта Yatube'
+        'posts': posts
     }
     return render(request, template, context)
 
